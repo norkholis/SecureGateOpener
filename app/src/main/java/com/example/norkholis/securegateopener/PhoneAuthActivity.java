1eb9dbdfbf3b9214875a0977d173e32574b72a89
norkholis <kholis.official@gmail.com>
2018-05-10 17:32:11 +0700

    Adding Some UI

---
+++ app/src/main/java/com/example/norkholis/securegateopener/PhoneAuthActivity.java
@@ -0,0 +1,13 @@
+package com.example.norkholis.securegateopener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_auth);
+    }
+}
